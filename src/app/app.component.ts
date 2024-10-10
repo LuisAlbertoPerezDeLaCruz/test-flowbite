93406f49c61bb08b8e0b9ef951dd113b36fc4855
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-10-10 12:03:20 -0400

    startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,10 @@ import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
-import { filter, map, Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { environment } from './environments/environment.development';
 
-export interface Suscriptor {
+export interface SuscriptorObject {
   id: number;
   firstName: string;
   lastName: string;
@@ -20,7 +18,7 @@ export interface Suscriptor {
 }
 
 export interface RootSuscriptorsObject {
-  users: Suscriptor[];
+  users: SuscriptorObject[];
   total: number;
   skip: number;
   limit: number;
@@ -38,53 +36,15 @@ export const ITEMS_PER_PAGE = 10;
 export class AppComponent implements OnInit {
   title = 'test-flowbite';
   ready = false;
-  $items: Observable<number[]> = of([1, 2, 3]);
-  items: number[] = [];
-  private http = inject(HttpClient);
-  private baseUrl: string = `${environment.API_URL}/users`;
-  private start = signal(0);
-  private end = signal(0);
-  private total = signal(0);
+  public $items: Observable<any> | null = null;
 
-  public users = signal<Suscriptor[]>([]);
-  public currentPage = signal(1);
   ngOnInit(): void {
     initFlowbite();
-    this.loadPage(this.currentPage());
+    this.loadPage();
+  }
+
+  loadPage() {
+    this.$items = of([1, 2, 3, 4, 5, 6, 7, 8, 9]);
     this.$items.subscribe();
-  }
-
-  loadPage(page: number, searchValue: string = '') {
-    if (page < 1) {
-      return;
-    }
-    this.currentPage.set(page);
-    this.getSuscriptors(page, searchValue)
-      .pipe(filter((res) => res.users.length > 0))
-      .subscribe((res: RootSuscriptorsObject) => {
-        this.users.set(res.users);
-        this.total.set(res.total);
-        this.start.set(res.skip + 1);
-        this.end.set(res.skip + res.limit);
-      });
-  }
-
-  getSuscriptors(
-    page: number = 1,
-    search: string = ''
-  ): Observable<RootSuscriptorsObject> {
-    const skip: number = (page - 1) * ITEMS_PER_PAGE;
-    const url: string = `${this.baseUrl}/search`;
-    return this.http
-      .get<RootSuscriptorsObject>(url, {
-        params: {
-          skip: skip,
-          limit: ITEMS_PER_PAGE,
-          select:
-            'id,firstName,lastName,email,gender,age,image,phone,birthDate',
-          q: search,
-        },
-      })
-      .pipe(map((response) => response));
   }
 }
