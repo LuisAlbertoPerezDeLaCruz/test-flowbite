9469b4b209964fb43157688b38da8fe1674fca2c
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-10-11 05:51:28 -0400

    final to be shared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,49 +2,33 @@ import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
-import { Observable, of } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
-
-export interface SuscriptorObject {
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
-  gender: string;
-  image: string;
-  phone: string;
-  age: number;
-  birthDate: Date;
-}
-
-export interface RootSuscriptorsObject {
-  users: SuscriptorObject[];
-  total: number;
-  skip: number;
-  limit: number;
-}
-
-export const ITEMS_PER_PAGE = 10;
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title = 'test-flowbite';
   ready = false;
-  public $items: Observable<any> | null = null;
 
-  ngOnInit(): void {
+  public items: any[] = [];
+
+  async ngOnInit() {
     initFlowbite();
-    this.loadPage();
+    await this.loadPage(1000);
   }
 
-  loadPage() {
-    this.$items = of([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    this.$items.subscribe();
+  async loadPage(delay = 0) {
+    if (delay > 0) await this.sleep(delay);
+    this.items.push(...[1, 2, 3, 4, 5, 6, 7]);
+    return;
+  }
+
+  sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 }
