389c439d85f27f155e724eeecc34664179f31d5d
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-10-11 06:33:35 -0400

    ajuste final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
